c1ec17d086e174aff699348f1cc3a10e58237762
Sid Pandey <sid2065@gmail.com>
2019-11-19 19:57:23 +0100

    Updated title and nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -12,9 +12,11 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
+            <h1 className="App-title">Find Fibonacci Numbers</h1>
+            <p className="nav">
             <Link to="/">Home</Link>
             <Link to="/otherpage">Other Page</Link>
+            </p>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
